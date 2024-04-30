6c0d1544874fbc93e0092776c481d8b88fade85b
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-04-30 15:53:10 +0200

    [UPD] Aggiunti if per stampa diversa

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
 'use strict';
 
 // --Creo un ciclo che va avanati finchè i valore dell'elemento è compreso tra 1 e 100 e stampi il risultato in console
+// --Aggiungo gli if necessari per fare in modo che per i multipli di 3 stampi “Fizz” e per i multipli di 5 stampi “Buzz”
 for (let i = 1; i <= 100; i++) {
-    console.log(i)
-}  
+
+    // Se è dividsibile sia per 3 sia per 5 stampa FIZZBUZZ
+    if ((i % 3 == 0) && (i % 5 == 0)){
+        console.log('FizzBuzz')
+    }
+
+    // Se è divisibile per 5 stampa BUZZ
+    else if (i % 5 == 0) {
+        console.log('Buzz');
+    }
+
+    // Se è divisibile per 3 stampa FIZZ
+    else if (i % 3 == 0) {
+        console.log('Fizz');
+    }
+
+    // Per tutti gli altri numeri invece stamap il numero
+    else {
+        console.log(i);
+    }};  
 
