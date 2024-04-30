2847c76e78c2771decb4190d4291ee0ffd088438
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-04-30 15:41:11 +0200

    [UPD] Ciclo da 1 a 100

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 'use strict';
 
+// --Creo un ciclo che va avanati finchè i valore dell'elemento è compreso tra 1 e 100 e stampi il risultato in console
+for (let i = 1; i <= 100; i++) {
+    console.log(i)
+}  
 
