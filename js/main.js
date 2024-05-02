d11dcffe44e0a849ca87a7a957532ade6a350d19
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-05-02 11:29:51 +0200

    [FIX] Piccole correzioni post correzione in classe

--- js/main.js
+++ js/main.js
@@ -10,33 +10,31 @@ for (let i = 1; i <= 100; i++) {
     // Creo variabile per la creazione del list-item
     const li = document.createElement('li');
 
-    // Se è dividsibile sia per 3 sia per 5 stampa FIZZBUZZ
-    if ((i % 5 == 0) && (i % 3 == 0)) {
+    // Se è dividsibile sia per 3 sia per 5 stampa FIZZBUZZ e aggiungi classe di stile specifica
+    if (i % 5 === 0 && i % 3 == 0) {
         console.log('FizzBuzz')
         lista.append(li);
         li.append('FizzBuzz');
         li.classList.add("bg-fizzbuzz");
     }
 
-    // Se è divisibile per 5 stampa BUZZ
-    else if (i % 5 == 0) {
+    // Se è divisibile per 5 stampa BUZZ e aggiungi classe di stile specifica
+    else if (i % 5 === 0) {
         console.log('Buzz');
         lista.append(li);
         li.append('Buzz');
         li.classList.add("bg-buzz");
-
     }
 
-    // Se è divisibile per 3 stampa FIZZ
-    else if (i % 3 == 0) {
+    // Se è divisibile per 3 stampa FIZZ e aggiungi classe di stile specifica 
+    else if (i % 3 === 0) {
         console.log('Fizz');
         lista.append(li);
         li.append('Fizz');
         li.classList.add("bg-fizz");
-
     }
 
-    // Per tutti gli altri numeri invece stampa il numero
+    // Per tutti gli altri numeri invece stampa il numero senza aggiungere nessuna classe di stile in più
     else {
         console.log(i);
         lista.append(li);
